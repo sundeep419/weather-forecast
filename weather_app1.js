55600cd41250114eecfe7a0d149e35608263aae9
sundeep419 <sundeep419@gmail.com>
2017-02-09 14:38:47 +0530

    almost completed

--- weather_app1.js
+++ weather_app1.js
@@ -37,73 +37,76 @@ $("#current").on("click",function(){
 	$.ajax({
 		url: urlString,
 		success: function(fore){
-			console.log(fore);
+			console.log(fore.list.length);
 				var startDate=[];
-				var futTemp=[];
-			for(var i=0; i< fore.list.length; i++){
+				var futTemp;
+				var a = fore.list.length;
+			for(var i=0; i < a; i++){
+			console.log(i);
 				startDate.push(moment((fore.list[i].dt )*1000).format("MMM Do YY"));
-				futTemp.push(fore.list[i].main.temp - 273);}
-				// var futHumid=fore.list[i].main.humidity;
-				// var futDescrip=fore.list[i].weather[0].description;
+				futTemp=Math.floor(fore.list[i].main.temp - 273);
+				var futHumid=fore.list[i].main.humidity || 0;
+				var futDescrip=fore.list[i].weather[0].description;
 				// console.log(startDate,futTemp,futHumid,futDescrip);
-				// var htmlNewTable='<tr><td>' + startDate + /*'</td><td>' + endDate +*/ '</td><td>' + futTemp + '</td><td>' + futHumid + '</td><td>' + futDescrip + '</td></tr>';
-				// $("tbody").append(htmlNewTable);
-				 Highcharts.chart('chart', {
-        
-				// included charts from highcharts for graphical representation of weather forecast
-				//creating variable to save in an array to show charts
-					// var dateArr= info.list.map(item => item.dt_txt);
-					// var tempArr= info.list.map(item => itme.main.temp);
-        chart: {
-            type: 'spline'
-        },
-        title: {
-            text: '5 Day Average Temperature'
-        },
-        subtitle: {
-            text: 'Source: openweathermap.org'
-        },
-        xAxis: { 
-            categories: startDate
-        },
-        yAxis: {
-            title: {
-                text: 'Temperature'
-            },
-            labels: {
-                formatter: function () {
-                    return this.value + '°';
-                }
-            }
-        },
-        tooltip: {
-            crosshairs: true,
-            shared: true
-        },
-        plotOptions: {
-            spline: {
-                marker: {
-                    radius: 4,
-                    lineColor: '#666666',
-                    lineWidth: 1
-                }
-            }
-        },
-        series: [{
-           
-            name: cityName,
-            marker: {
-                symbol: 'diamond'
-            },
-            data: futTemp
-        }]
-    });
-			
+			var htmlTable='<tr><td>' + startDate + '</td><td>' + futTemp + '</td><td>' + futHumid + '</td><td>' + futDescrip + '</td></tr>';
+				$("tbody").append(htmlTable);
+				startDate.pop(moment((fore.list[i].dt )*1000).format("MMM Do YY"));
+			}
 		},
 		error:function(fore){
 			console.log("Err",fore);
 		}
-
+	});
 	});	
 	});
-});
+		// 		 Highcharts.chart('chart', {
+        
+		// 		// included charts from highcharts for graphical representation of weather forecast
+		// 		//creating variable to save in an array to show charts
+		// 			// var dateArr= info.list.map(item => item.dt_txt);
+		// 			// var tempArr= info.list.map(item => itme.main.temp);
+  //       chart: {
+  //           type: 'spline'
+  //       },
+  //       title: {
+  //           text: '5 Day Average Temperature'
+  //       },
+  //       subtitle: {
+  //           text: 'Source: openweathermap.org'
+  //       },
+  //       xAxis: { 
+  //           categories: startDate
+  //       },
+  //       yAxis: {
+  //           title: {
+  //               text: 'Temperature'
+  //           },
+  //           labels: {
+  //               formatter: function () {
+  //                   return this.value + '°';
+  //               }
+  //           }
+  //       },
+  //       tooltip: {
+  //           crosshairs: true,
+  //           shared: true
+  //       },
+  //       plotOptions: {
+  //           spline: {
+  //               marker: {
+  //                   radius: 4,
+  //                   lineColor: '#666666',
+  //                   lineWidth: 1
+  //               }
+  //           }
+  //       },
+  //       series: [{
+           
+  //           name: cityName,
+  //           marker: {
+  //               symbol: 'diamond'
+  //           },
+  //           data: futTemp
+  //       }]
+  //   });
+			
